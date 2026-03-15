b40596addc3f0a17b5792a5eb52f41b2b74363d7
rsy <nathan.riley.sy@student.ateneo.edu>
2026-03-15 21:04:54 +0800

    cleaning up for proj hehe

--- M5E-DjangoTemplates-HerreraLatonioSy/tapasapp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Dish
-
-# Register your models here.
-admin.site.register(Dish)
--- M5E-DjangoTemplates-HerreraLatonioSy/tapasapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TapasappConfig(AppConfig):
-    name = 'tapasapp'
